2b9b8009625b7639914cfc4bf9850e3fb553cf03
nightRainy <44838179+nightRainy@users.noreply.github.com>
2019-07-17 15:35:11 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,16 @@ if [ `whoami` = "root" ];then
 	apt-get install git -y
 	apt-get install zsh -y
 	apt-get install wget -y
-	apt-get install libc6-dev-i386
+	apt-get install gem -y
+	apt-get install ruby -y
+	apt-get install libc6-dev-i386 -y
+	gem install one_gadget
 	pip install pwntools
 	git clone https://github.com/pwndbg/pwndbg.git
 	cd pwndbg 
 	./setup.sh
-	cd ..
+	cd 
+	git clone https://github.com/inaz2/roputils.git
 	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
 	cd
 	sed -i 's|robbyrussell|bira|' .zshrc
@@ -27,12 +31,16 @@ else
 	sudo apt-get install git -y
 	sudo apt-get install zsh -y
 	sudo apt-get install wget -y
-	sudo apt-get install libc6-dev-i386
+	sudo apt-get install gem -y
+	sudo apt-get install ruby -y
+	sudo apt-get install libc6-dev-i386 -y
+	sudo gem install one_gadget
 	pip install pwntools
 	git clone https://github.com/pwndbg/pwndbg.git
 	cd pwndbg 
 	./setup.sh
-	cd ..
+	cd 
+	git clone https://github.com/inaz2/roputils.git
 	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
 	cd
 	sed -i 's|robbyrussell|bira|' .zshrc
