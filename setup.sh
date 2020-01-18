10fe1e11930719225a5769e436e24966543dc637
知世 <44838179+nightRainy@users.noreply.github.com>
2020-01-18 16:33:35 +0800

    update libcsearcher
    libcsearcher

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ if [ `whoami` = "root" ];then
 	apt-get install gcc -y
 	apt-get install gdb -y
 	apt-get install git -y
-	apt-get install zsh -y
 	apt-get install wget -y
 	apt-get install gem -y
 	apt-get install ruby -y
 	apt-get install tmux -y
 	apt-get install libc6-dev-i386 -y
+	apt-get -y install netcat-traditional
 	gem install one_gadget
 	pip install pwntools
 	git clone https://github.com/pwndbg/pwndbg.git
@@ -19,10 +19,10 @@ if [ `whoami` = "root" ];then
 	./setup.sh
 	cd 
 	git clone https://github.com/inaz2/roputils.git
-	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
-	cd
-	sed -i 's|robbyrussell|bira|' .zshrc
-	source .zshrc
+	cd 
+	git clone https://github.com/lieanu/LibcSearcher.git
+	cd LibcSearcher
+	python setup.py develop
 else
 	sudo apt-get update -y
 	sudo apt-get install python -y
@@ -30,11 +30,11 @@ else
 	sudo apt-get install gcc -y
 	sudo apt-get install gdb -y
 	sudo apt-get install git -y
-	sudo apt-get install zsh -y
 	sudo apt-get install wget -y
 	sudo apt-get install gem -y
 	sudo apt-get install ruby -y
 	sudo apt-get install tmux -y
+	sudo apt-get -y install netcat-traditional
 	sudo apt-get install libc6-dev-i386 -y
 	sudo gem install one_gadget
 	pip install pwntools
@@ -43,10 +43,10 @@ else
 	./setup.sh
 	cd 
 	git clone https://github.com/inaz2/roputils.git
-	sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" -y
 	cd
-	sed -i 's|robbyrussell|bira|' .zshrc
-	source .zshrc
+	git clone https://github.com/lieanu/LibcSearcher.git
+	cd LibcSearcher
+	python setup.py develop
 fi
 
 
